d397cced68f382ba86b430764d51889a4021bf08
Hugh Saffar <hughsaffar@gmail.com>
2021-02-02 22:53:17 -0500

    0.1.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 All notable changes to `larawatcher` will be documented in this file
 
+## 0.1.3 - 2021-02-02
+
+- Fix issue with path replacer.
+
 ## 0.1.2 - 2021-02-02
 
 - Remove model hydration details
--- src/Entities/BaseEntity.php
+++ src/Entities/BaseEntity.php
@@ -17,7 +17,7 @@ abstract class BaseEntity
     {
         return collect($backtrace->frames())
             ->filter(
-                fn (Frame $frame) => ! Str::contains($frame->file, [
+                fn(Frame $frame) => !Str::contains($frame->file, [
                     '/vendor/',
                     'Larawatcher.php',
                     '/laravel-db-profiler/',
@@ -29,15 +29,13 @@ abstract class BaseEntity
 
     protected function getFile(Frame $frame)
     {
-        $appPath = config('larawatcher.app_path') ?: base_path();
-
-        return Str::replaceFirst($appPath, '', $frame->file);
+        return Str::replaceFirst(base_path(), '', $frame->file);
     }
 
     protected function getBacktrace(): Collection
     {
         return $this->frames->map(
-            fn (Frame $frame) => [
+            fn(Frame $frame) => [
                 'file' => $this->getFile($frame),
                 'line' => $frame->lineNumber,
                 'code' => $frame->getSnippet(5),
